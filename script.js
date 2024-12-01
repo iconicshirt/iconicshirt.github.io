add0085e2d0f2c59286fde3af39e373ae3a18b12
iconicshirt <iconicshirt048@gmail.com>
2024-12-01 13:54:26 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById('comandeBtn').addEventListener('click', function() {
+    // Redirigir al cliente al formulario de Google Forms
+    window.location.href = "https://forms.gle/GCAPX1TLrq6n3owH7";
+});
+
+// Cuando el formulario es enviado, el redireccionamiento será manejado automáticamente por Google Forms.
+// No se necesita código adicional aquí para manejar el retorno al sitio web, ya que Google Forms generalmente redirige
+// al usuario a una página de agradecimiento o vuelve a cargar el sitio automáticamente después de completar el formulario.
